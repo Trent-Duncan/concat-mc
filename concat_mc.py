686eac3a36b740e0d5773c6a1bafae14e31a5ccf
Trent-Duncan <duncantr131@northwested.org>
2024-09-20 14:06:01 -0400

    Add files via upload

--- concat_mc.py
+++ concat_mc.py
@@ -1,1 +1,30 @@
-print('Hello!')
+# Trent Duncan
+# 9/20/24
+# Concatenation Check
+
+
+# Task One
+first_name= 'Trent '
+fav_junk_food= 'Pizza'
+# first_sentence= f"Hi my name is {first_name}"
+
+first_sentence = "Hi my name is " + first_name
+
+
+#second_sentence= f"and my favorite food is {fav_junk_food}"
+
+second_sentence= "and my favorite food is " + fav_junk_food + "."
+
+full_sentence= first_sentence + second_sentence
+print(full_sentence)
+
+# Task Two
+print(" What I've learned about concatenation: ")
+print("Concatenation means 'to join'. ")
+print("With it you can break a phrase into multipule pieces then combine them into one full sentence")
+
+# Task Three
+# burger= 'Double Bacon Cheese Burger costs '
+price = 6.99
+# sentence=  burger + price
+print ("At Burger King a Double Bacon Cheese Burger costs: " + "$" + str(price) + ".")
